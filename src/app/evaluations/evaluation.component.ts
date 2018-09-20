72301f3dd340bbb578bb3d8a8389970b0a878481
Guilherme Nass <nassguilherme@gmail.com>
2018-09-20 19:11:03 -0300

    ajuste templateUrl

--- src/app/evaluations/evaluation.component.ts
+++ src/app/evaluations/evaluation.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'evaluation-component',
-  templateUrl: './evaluation.component'
+  templateUrl: './evaluation.component.html'
 })
 
 export class EvaluationComponent {

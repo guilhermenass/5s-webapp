418bd639a65b8e16b342fcdc78507c6481cb104b
Guilherme Nass <nassguilherme@gmail.com>
2018-10-18 14:50:12 -0300

    removi as barras da apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api-5s.herokuapp.com/",
+  apiUrl: "https://api-5s.herokuapp.com",
   webappUrl: 'https://web-5s.herokuapp.com',
-  loginUrl: 'https://login-5s.herokuapp.com/'
+  loginUrl: 'https://login-5s.herokuapp.com'
 };

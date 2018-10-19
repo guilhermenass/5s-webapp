d67a0e9cf211e8ea6b109ae99c030c0d698c70f2
Guilherme Nass <nassguilherme@gmail.com>
2018-10-19 10:37:53 -0300

    ajustes

--- src/app/audits/audit.ts
+++ src/app/audits/audit.ts
@@ -3,7 +3,7 @@ import { Evaluation } from "./evaluation";
 export class Audit {
     id: number;
     title: string;
-    unit_id: number;
+    units_id: number;
     unit_name: string;
     evaluations: Array<Evaluation>;
     initial_date: Date;
@@ -15,7 +15,7 @@ export class Audit {
         initial_date?: Date, due_date?: Date, description?: string, status: number = 0, id?: number){
         this.id = id;
         this.title = title;
-        this.unit_id = units_id;
+        this.units_id = units_id;
         this.evaluations = evaluations;
         this.initial_date = initial_date;
         this.due_date = due_date;
